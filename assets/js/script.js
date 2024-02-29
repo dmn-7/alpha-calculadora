1d0dca286362dd9037798a01f3a1132b20d9f493
dmn-7 <161778772+dmn-7@users.noreply.github.com>
2024-02-29 20:27:17 -0300

    Upload site calculadora alpha

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*comando para fazer o calculo ao clicar no botao calculate */
+document.getElementById('calculate').addEventListener('click', function(){
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+
+    const total = (value1 / value2);
+    const total2 = (value1-((Math.floor(total))* value2));
+
+    document.getElementById('total').innerHTML=(Math.floor(total) + " - PALLET COMPLETOS ( Obs: Cada pallet completo tem " + value2 + " caixas )")
+    document.getElementById('total2').innerHTML=(Math.floor(total2) + " - CAIXA ")
+});
+
+/* comando ao clicar no botao clean ele vai limpar os campos */
+document.getElementById('clean').addEventListener('click', function(){
+    document.getElementById('value1').value="";
+    document.getElementById('value2').value="";
+
+    document.getElementById('total').innerHTML="";
+    document.getElementById('total2').innerHTML="";
+
+   /*comando para o cursor ir para o primeiro campo ao clicar no botao clean */ 
+    document.getElementById('value1').focus();
+});
